b1d19ee99b0ab0cb8e9e2394c1ef2598487fb028
Joe Arcelo <josepharcelo@gmail.com>
2018-11-11 10:03:29 -0600

    Add multiple tests using struct

--- convert_test.go
+++ convert_test.go
@@ -3,9 +3,25 @@ package main
 import "testing"
 
 func TestConvertDigitsToWords(t *testing.T)  {
-	expected := "two"
-	actual := ConvertDigitsToWords(2)
-	if actual != expected {
-		t.Errorf("Test failed.")
+	var tests = []struct {
+		in int
+		out string
+	}{
+		{2, "two"},
+		{10, "ten"},
+		{100, "one hundred"},
+		{999, "nine hundred ninety nine"},
+		{1010, "one thousand ten"},
+		{9999, "nine thousand nine hundred ninety nine"},
+		{99999, "ninety nine thousand nine hundred ninety nine"},
+		//{999999, "nine hundred ninety nine thousand nine hundred ninety nine"},  //TODO: This test fails
+		{1000000, "one million"},
+	}
+
+	for _, test := range tests {
+		actual := ConvertDigitsToWords(test.in)
+		if actual != test.out {
+			t.Errorf("Test failed. Expected: %v; Actual: %s", test.out, actual)
+		}
 	}
 }
